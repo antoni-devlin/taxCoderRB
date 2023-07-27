55336fb252982b2034efc54c69e168889de9f5d1
Antoni <antoni-devlin@users.noreply.github.com>
2023-07-27 14:47:42 +0000

    Made changes to tax code solving logic in script.rb: - getUsersTaxCode() now takes a userCode argument and returns it - checkCode() now builds an array of outcomes relevant to the user's taxcode, and returns it - getPersonalAllowance now returns the personal allowance as an int
    Also removed main() as it's no longer needed.

--- script.rb
+++ script.rb
@@ -3,17 +3,19 @@ require 'yaml'
 $taxcodes = YAML.load_file('taxcodes.yml')
 $emergencyCodes = YAML.load_file('taxcodes.yml')
 
-def getUsersTaxCode()
-  usersCode = gets
+def getUsersTaxCode(usersCode)
+  #TODO: Add validation taxcode validation here
   return usersCode
 end
 
 def checkCode(taxCode)
+  outcomes = []
   $taxcodes.each { |item|
     if taxCode.match(item["code"])
-      puts "As your tax code contains #{item["code"]}, #{item["description"]}"
+      outcomes.append("As your tax code contains #{item["code"]}, #{item["description"]}")
     end 
   }
+  return outcomes
 end
 
 def checkEmergencyCode(taxCode)
@@ -27,14 +29,5 @@ end
 def getPersonalAllowance(taxCode)
   personalAllowance = taxCode[/\d{3,}/,0]
   personalAllowance = personalAllowance.to_i * 10
-  # personalAllowance.insert(2, ",")
-  puts("Based on the information you have provided, your personal allowance is £#{personalAllowance}")
+  return personalAllowance
 end
-
-def main()
-  usersTaxCode = getUsersTaxCode
-  checkCode(usersTaxCode)
-  getPersonalAllowance(usersTaxCode)
-end
-
-main
