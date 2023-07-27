c090868806c6edf8a8ea88bf276194fcb75f8547
Antoni <antoni-devlin@users.noreply.github.com>
2023-07-27 09:12:37 +0000

    Created a basic sinatra app. This includes: - routes - base, index, outcome and taxcode_form view

--- app.rb
+++ app.rb
@@ -2,7 +2,23 @@
 
 require 'sinatra'
 require "sinatra/reloader" if development?
+set :static, true
+set :public_folder, "static"
+set :views, "views"
+
+before do
+  @pageTitle = "TaxSolver"
+end
 
 get '/' do
-  "Hello world!"
+  erb :index, :layout => :_base do
+    erb :taxcode_form
+  end
+end
+
+post '/tax-code-form/' do
+  #Render template
+  erb :index, :layout => :_base do
+     erb :outcome
+   end
 end
